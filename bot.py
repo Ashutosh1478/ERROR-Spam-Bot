6a94d98f065d94b947872dfebce002335ee8cd73
CyrusXD <79554993+Ashutosh1478@users.noreply.github.com>
2022-04-14 09:35:30 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -570,6 +570,11 @@ async def spam(e):
 async def _(event):
  if e.sender_id in SMEX_USERS:
         return await event.reply("...")
+
+async def _(event):
+    if event.fwd_from or event.via_bot_id:
+        return
+    s_m_ = await event.reply("prom...")
     cmd = event.raw_text.split(" ", maxsplit=1)[1]
     reply_to_id = event.message.id
     if event.reply_to_msg_id:
